e03ebb7e72b1c125266d6efc00d88e5a3bc21ac0
Josh Schmitz <joshschmitz8.8@gmail.com>
2021-05-20 13:42:50 -0600

    solving algorithm finished

--- main.py
+++ main.py
@@ -11,50 +11,63 @@ board = [
 ]
 
 def printBoard(board):
-	for row in board:
-		print(row)
-
-printBoard(board)
+	for i in range(9):
+		if i % 3 == 0 and i != 0:
+			print("---+---+---")
+		for j in range(9):
+			if j % 3 == 0 and j != 0:
+				print("|" + str(board[i][j]), end="")
+			elif j == 8:
+				print(board[i][j])
+			else:
+				print(board[i][j], end="")			
 
 def solveBoard(board):
-	i,j = findEmpty(board)
+	if not findEmpty(board):
+		return True
+	else:
+		i, j = findEmpty(board)
 	
+	for n in range(1, 10):
+		if isValid(board, n, i, j):
+			board[i][j] = n
+			
+			if solveBoard(board):
+				return True
+			
+			board[i][j] = 0
+	return False
+
 
 def findEmpty(board):
 	for i in range(9):
 		for j in range(9):
 			if board[i][j] == 0:
 				return i,j				
+	return None
 
-def isValid(board, i, j):
+def isValid(board, val, i, j):
 	for x in range(9): # check row
-		if board[i][x] == board[i][j] and x != j:
-			return false
+		if board[i][x] == val:
+			return False
 	for y in range(9): # check column
-		if board[y][j] == board[i][j] and y != i:
-			return false
-	return checkSquare(board, i, j) # check square
+		if board[y][j] == val:
+			return False
+	return checkSquare(board, val, i, j)
 
-def checkSquare(board, i, j):
-	if i < 3:
-		if j < 3: # top left
-			# do something
-		elif j < 6: # top middle
-			# do something
-		else: # top right
-			# do something
-	elif i < 6:
-		if j < 3: # middle left
-			# do something
-		elif j < 6: # middle middle
-			# do something
-		else: # middle right
-			#do something
-	else:
-		if j < 3: # bottom left
-			# do something
-		elif j < 6: # bottom middle
-			# do something
-		else: # bottom right
-			# do something
-		
+def checkSquare(board, val, i, j):
+	square_x = j // 3
+	square_y = i // 3
+
+	for y in range(square_y * 3, square_y * 3 + 3):
+		for x in range(square_x * 3, square_x * 3 + 3):
+			if board[y][x] == val:
+				return False
+	return True
+
+printBoard(board)
+print()
+
+solveBoard(board)
+
+printBoard(board)
